e4ad8f9945f133af3108aa6c99913d1a8d964531
William Michelli <will.d.m@outlook.com>
2025-11-20 21:13:22 -0300

    ajuste

--- job-frontend/src/app/enviroment/enviroment.ts
+++ job-frontend/src/app/enviroment/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5294/api/'
-  //baseUrl: 'https://servicolinux-epgcchhfetabakda.brazilsouth-01.azurewebsites.net/api/'
+  //baseUrl: 'http://localhost:5294/api/'
+  baseUrl: 'https://centraloportunidades.onrender.com/api/'
 };
